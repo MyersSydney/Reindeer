642cf246f14552b17cb4d1884dd03bbf697c5074
Flare26 <47835828+Flare26@users.noreply.github.com>
2020-12-05 05:02:05 -0600

    Added UI and santa knocks stuff around

---
+++ Assets/UIManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public Text distDisplay;
+    GameManager gm;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        gm = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        distDisplay.text = string.Format("Current Run: {0:0.00}m\nBest Run: {1:0.00}m ", gm.playerDist, gm.bestDist);
+    }
+}
