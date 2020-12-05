dabacd909a0608845bfd06b0b3f03b579373943b
squidmyers33 <squidmyers33@gmail.com>
2020-12-04 23:59:56 -0600

    i think i mihgt kill myself tonight nathan

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public float maxTimer = 1;
+    private float timer = 0;
+    public GameObject[] pipe;
+    public float height;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject newPipe = Instantiate(pipe[UnityEngine.Random.Range(0, 11)]);
+        newPipe.transform.position = transform.position + new Vector3(0, Random.Range(-height, height), 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if(timer > maxTimer)
+        {
+            GameObject newPipe = Instantiate(pipe[UnityEngine.Random.Range(0, 11)]);
+            newPipe.transform.position = transform.position + new Vector3(0, Random.Range(-height, height), 0);
+            Destroy(newPipe, 15);
+            timer = 0;
+        }
+
+        timer += Time.deltaTime;
+    }
+}
