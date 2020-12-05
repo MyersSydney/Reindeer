a4ee3982ad6db707269ca2968fec6b0d8ecdd956
squidmyers33 <squidmyers33@gmail.com>
2020-12-04 23:11:23 -0600

    :)

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float velocity = 1;
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            rb.velocity = Vector2.up * velocity;
+        }
+    }
+}
---
+++ Assets/Scripts/MoveCamera.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Vector3 mousePosition;
+    public float moveSpeed = 0.1f;
+    Vector3 worldPosition;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.y = 10;
+        worldPosition = Camera.main.ScreenToWorldPoint(mousePos);
+    }
+}
